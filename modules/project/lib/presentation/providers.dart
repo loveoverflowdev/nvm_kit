0e38d1c89d45ae6334fec865a3cc0dbf26a3e406
Pham Duc Manh <manhpdbiz@gmail.com>
2024-08-23 00:38:08 +0700

    stash

--- modules/project/lib/presentation/providers.dart
+++ modules/project/lib/presentation/providers.dart
@@ -2,3 +2,4 @@ export 'providers/project_repository_provider.dart';
 export 'providers/project_input_provider.dart';
 export 'providers/project_list_provider.dart';
 export 'providers/project_submit_provider.dart';
+export 'providers/workspace_id_provider.dart';
